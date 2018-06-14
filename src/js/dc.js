2c92c5b77abc04026323fb6e4118e0440303b145
Wayne Stedman <wstedman@dhccare.com>
2018-06-14 16:00:58 -0700

    product slider 1st draft

--- src/js/dc.js
+++ src/js/dc.js
@@ -1,41 +1,50 @@
 $(document).ready(function(){
-  $('.ingredientsCarousel').slick({
-    dots: false,
+  $("#productsSlider").slick({
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    cssEase: 'ease-in-out',
+    dots: true,
+    fade: true,
     infinite: true,
-    speed: 600,
-    slidesToShow: 6,
-    slidesToScroll: 3,
-    responsive: [
-      {
-        breakpoint: 1280,
-        settings: {
-          slidesToShow: 4,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: false
-        }
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
+    pauseOnDotsHover: true,
+    speed: 1200,
+    // slidesToShow: 1,
+    // slidesToScroll: 1,
+    customPaging: function(slider, i) {
+        var thumbnail = $(slider.$slides[i]).data("thumbnail");
+        return '<a href="javascript:void(0)"><img src="'+thumbnail+'"></a>'}
   });
-
-  // var videoModal = $('#video').modal();
-  // $('#openVideo').on('click', function(e) {
-  //   e.preventDefault();
-  //   videoModal.modal('openModal');
-  //  $("#simple-iframe").attr('src', "https://fast.wistia.net/embed/iframe/ozmj3dq5tp?videoFoam=true&autoplay=1");
+  // $('.setsCarousel').slick({
+  //   dots: false,
+  //   infinite: true,
+  //   speed: 600,
+  //   slidesToShow: 3,
+  //   slidesToScroll: 3,
+  //   responsive: [
+  //     {
+  //       breakpoint: 1280,
+  //       settings: {
+  //         slidesToShow: 3,
+  //         slidesToScroll: 3,
+  //         infinite: true,
+  //         dots: false
+  //       }
+  //     },
+  //     {
+  //       breakpoint: 768,
+  //       settings: {
+  //         slidesToShow: 2,
+  //         slidesToScroll: 2
+  //       }
+  //     },
+  //     {
+  //       breakpoint: 600,
+  //       settings: {
+  //         slidesToShow: 1,
+  //         slidesToScroll: 1
+  //       }
+  //     }
+  //   ]
   // });
-}); // doc ready
+});
