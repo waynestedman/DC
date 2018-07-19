da180e76962eb9f8f66edb2f1a0c26c3f9cc01f5
Wayne Stedman <wstedman@dhccare.com>
2018-07-18 17:46:27 -0700

    working on mobile veiew; 2/3 drafted.

--- src/js/server-ver.js
+++ src/js/server-ver.js
@@ -1,49 +1,62 @@
 <script type="text/javascript" xml="space">// <![CDATA[
-
 require([
   'jquery',
   'slick',
   'Magento_Ui/js/modal/modal'
   ], function ($) {
-    $(document).ready(function () {
-      var videoModal = $('#video').modal();
-      $(".productCarousel").slick({
-        dots: false,
-        speed: 600,
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        responsive: [
-          {
-            breakpoint: 1280,
-            settings: {
-              slidesToShow: 3,
-              slidesToScroll: 3,
-              infinite: true,
-              dots: false
-            }
-          },
-          {
-            breakpoint: 768,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 2
-            }
-          },
-          {
-            breakpoint: 600,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
-          }
-        ]
-      });
-      $('#openVideo').on('click', function(e) {
+$(document).ready(function(){
+  var videoModal = $('#video').modal();
+  $("#productsSlider").slick({
+    arrows: false,
+    autoplay: false,
+    autoplaySpeed: 4000,
+    cssEase: 'ease-in-out',
+    dots: true,
+    fade: true,
+    infinite: true,
+    pauseOnDotsHover: true,
+    speed: 1200,
+    customPaging: function(slider, i) {
+        var thumbnail = $(slider.$slides[i]).data("thumbnail");
+        return '<a href="javascript:void(0)"><img src="'+thumbnail+'" class="img-wrapper"></a>'}
+  });
+ $('#setsCarousel').slick({
+    dots: false,
+    infinite: true,
+   speed: 600,
+    slidesToShow: 5,
+    slidesToScroll: 5,
+    responsive: [
+      {
+        breakpoint: 1280,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 5,
+          infinite: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+  $('#openVideo').on('click', function(e) {
         e.preventDefault();
         videoModal.modal('openModal');
-       $("#simple-iframe").attr('src', "https://fast.wistia.net/embed/iframe/ozmj3dq5tp?videoFoam=true&autoplay=1");
+      $("#simple-iframe").attr('src', "https://fast.wistia.net/embed/iframe/s0w5d5rgpa?videoFoam=true&autoplay=1");
       });
     });
   });
-]]></script>
+// ]]></script>
